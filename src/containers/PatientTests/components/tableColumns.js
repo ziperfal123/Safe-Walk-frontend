1cbf647062aac8a022328ae8af381602d4e846bf
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-03-10 00:44:21 +0200

    table building- pretty much done

--- src/containers/PatientTests/components/tableColumns.js
+++ src/containers/PatientTests/components/tableColumns.js
@@ -1,43 +1,72 @@
 import React from "react";
 import { normalizeDate } from '../../../utils/date'
+
+const MARKED_ROW_BACKGROUND_COLOR = 'hsl(0, 98%, 84%)';
 const columns = [
     {
         title: 'Patient name',
         dataIndex: 'name',
-        render: (_ , normalizedPatientObj) => {
-            return (
-                <div>
-                    {normalizedPatientObj.image &&
+        render( _ , normalizedPatientObj) {
+            const rowColor = normalizedPatientObj.waitForPlan === 'yes' ? `${MARKED_ROW_BACKGROUND_COLOR}` : ''
+            return {
+                props: {
+                    style: {background: `${rowColor}`},
+                },
+                children:(
+                    <div>
+                        {normalizedPatientObj.image &&
                         <img
                             src= {normalizedPatientObj.image}
                             alt='patient image'
                             style={{width: '45px', height:'45px'}}
                         />}
-                    {` ${normalizedPatientObj.name}`}
-                </div>
-            )}
+                        {` ${normalizedPatientObj.name}`}
+                    </div>
+                ),
+            }
+        }
     },
     {
         title: 'Test date',
         dataIndex: 'testDate',
-        render: testDate => {
-            let normalizedDate = normalizeDate(testDate)
-            return <span>{normalizedDate}</span>
+        render: (testDate, normalizedPatientObj) => {
+            const normalizedDate = normalizeDate(testDate)
+            const rowColor = normalizedPatientObj.waitForPlan === 'yes' ? `${MARKED_ROW_BACKGROUND_COLOR}` : ''
+            return {
+                props: {
+                    style: {background: `${rowColor}`},
+                },
+                children: (<span>{normalizedDate}</span>)
+            }
         }
     },
     {
         title: 'Results',
         dataIndex: 'results',
-        render: results => {
+        render: (results, normalizedPatientObj) => {
             const backgroundColorClass = results === 'abnormality' ? 'green' : 'red'
-                return <span className={`results ${backgroundColorClass}`}>{results}</span>
-        },
+            const rowColor = normalizedPatientObj.waitForPlan === 'yes' ? `${MARKED_ROW_BACKGROUND_COLOR}` : ''
+            return {
+                props: {
+                    style: {background: `${rowColor}`},
+                },
+                children: (<span className={`results ${backgroundColorClass}`}>{results}</span>)
+            }
+        }
     },
     {
         title: 'wait for plan',
         dataIndex: 'waitForPlan',
-        render: waitingStatus => <span className={'waiting-status'}>{waitingStatus}</span>
-    },
+        render: (waitingStatus, normalizedPatientObj) => {
+            const rowColor = normalizedPatientObj.waitForPlan === 'yes' ? `${MARKED_ROW_BACKGROUND_COLOR}` : ''
+            return {
+                props: {
+                    style: {background: `${rowColor}`},
+                },
+                children: (<span className={'waiting-status'}>{waitingStatus}</span>)
+            }
+        }
+    }
 ];
 
 export default columns
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,23 @@
 export const normalizeDate = dateToNormalize => {
     let normalizedDate = new Date(dateToNormalize)
-    const day = normalizedDate.getDate()
-    const month = normalizedDate.getMonth()
-    const year = normalizedDate.getFullYear()
-    normalizedDate = `${day}-${month}-${year}`
+    let day = normalizedDate.getDate().toString()
+    day = addZeroPrefixIfNeeded(day)
+    let month = normalizedDate.getMonth().toString()
+    month = addZeroPrefixIfNeeded(month)
+    const year = normalizedDate.getFullYear().toString()
+    let hours = normalizedDate.getUTCHours().toString()
+    hours = addZeroPrefixIfNeeded(hours)
+    let minutes = normalizedDate.getUTCMinutes().toString()
+    minutes = addZeroPrefixIfNeeded(minutes)
+
+    normalizedDate = `${day}-${month}-${year}, ${hours}:${minutes}`
     return normalizedDate
+}
+
+
+const addZeroPrefixIfNeeded = strToModify => {
+    if (strToModify.length === 1) {
+        return '0' + strToModify
+    }
+    return strToModify
 }
