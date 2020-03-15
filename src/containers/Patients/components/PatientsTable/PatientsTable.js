f12cd89a227ade13f4c1874625f1d6054fb21615
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-03-15 15:22:50 +0200

    cont

--- src/containers/Patients/components/PatientsTable/PatientsTable.js
+++ src/containers/Patients/components/PatientsTable/PatientsTable.js
@@ -5,6 +5,11 @@ import columns from "./tableColumns"
 const PatientsTable = props => {
     console.log('PatientsTable')
 
+    function handleRowClick(record , index) {
+        return {
+            onClick: () => console.log('rcord: ', record)
+        }
+    }
     return (
         <div className={'table-wrapper'}>
             <Table
@@ -12,6 +17,7 @@ const PatientsTable = props => {
                 columns={columns}
                 dataSource={ props.allPatients}
                 pagination={false}
+                onRow={ handleRowClick }
             />
         </div>
     )
