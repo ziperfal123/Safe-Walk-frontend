f3da8f809c5d2625ce18e5991a6d2eed0250ae00
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-03-17 19:12:54 +0200

    start

--- src/containers/Patients/components/PatientPage/PatientPage.js
+++ src/containers/Patients/components/PatientPage/PatientPage.js
@@ -1,13 +1,24 @@
 import React from "react";
+import './patientPage.scss'
 
 const PatientPage = props => {
     console.log('PatientPage')
     console.log('props: ', props)
-    
+
+    const { patient } = props
     return (
-        <div>
-            <h1>{props.patient.name}</h1>
-            <button onClick={() => {props.history.push('/patients/')}}>Back</button>
+        <div className={'patient-page-container'}>
+            <div className={'left-section'}>
+                <img src={patient.picture}/>
+                <h2>{patient.mail}</h2>
+                <h2>{patient.name}</h2>
+                <h2>{patient.age} Years old</h2>
+                <button onClick={() => {props.history.push('/patients/')}}>Back</button>
+            </div>
+            <div className={'right-section'}>
+                <h1>Last Tests</h1>
+            </div>
+            <hr />
         </div>
     )
 }
