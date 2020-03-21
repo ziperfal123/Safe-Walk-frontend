bfc78b6ceb484a1d8295b1ee3bb6d626998a4d5b
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-03-21 17:29:09 +0200

    small routing fix

--- src/containers/Patients/Patients.js
+++ src/containers/Patients/Patients.js
@@ -10,13 +10,20 @@ const Patients = (props) => {
   console.log('Patients');
   console.log('props: ', props);
   const {
-    history, getAllPatients, getTestsById, allPatients, loadingAllPatients,loadingAllTestsById
+    location,
+    history,
+    getAllPatients,
+    getTestsById,
+    allPatients,
+    loadingAllPatients,
+    loadingAllTestsById,
   } = props;
 
   const [selectedPatient, setSelectedPatient] = useState('');
 
   useEffect(() => {
     getAllPatients();
+    if (selectedPatient === '' && location.pathname !== pathsNames.patients) history.push(pathsNames.patients);
   }, []);
 
   function handleTableRowClick(patientObj) {
@@ -38,6 +45,7 @@ const Patients = (props) => {
             />
           )}
         />
+        {selectedPatient && (
         <Route
           path={`${pathsNames.patients}:${selectedPatient.id}`}
           render={() => (
@@ -49,6 +57,7 @@ const Patients = (props) => {
           )}
           getTestsById={getTestsById}
         />
+        ) }
       </Switch>
     </div>
   );
@@ -58,6 +67,7 @@ export default Patients;
 
 Patients.propTypes = {
   history: PropTypes.objectOf(PropTypes.any).isRequired,
+  location: PropTypes.objectOf(PropTypes.any).isRequired,
   getAllPatients: PropTypes.func.isRequired,
   getTestsById: PropTypes.func.isRequired,
   allPatients: PropTypes.arrayOf(PropTypes.object).isRequired,
