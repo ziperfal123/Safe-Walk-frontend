3ea363792cc0ceae6a010cf022af3bd112e878ad
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-04-03 18:49:47 +0300

    login page refactoring- done

--- src/containers/Login/components/Form/Form.js
+++ src/containers/Login/components/Form/Form.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react';
-import PropTypes from 'prop-types';
-import './form.scss';
+import React, { useEffect, useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import './form.scss'
 
 const Form = ({ handleLoginFormSubmit }) => {
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
-  const firstInputRef = useRef();
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+  const firstInputRef = useRef()
 
   useEffect(() => {
-    firstInputRef.current.focus();
-  }, []);
+    firstInputRef.current.focus()
+  }, [])
 
   function handleInputChange(event) {
-    const { value } = event.target;
-    const { name } = event.target;
-    name === 'mail' ? setMail(value) : setPassword(value);
+    const { value } = event.target
+    const { name } = event.target
+    name === 'mail' ? setMail(value) : setPassword(value)
   }
 
   function handleLoginClick(e) {
-    e.preventDefault();
-    handleLoginFormSubmit();
+    e.preventDefault()
+    handleLoginFormSubmit()
   }
 
   return (
-    <form className="form-container">
-      {/* <i className="fas fa-user"></i> */}
+    <form className="login-form">
       <input
         type="text"
         placeholder="user name"
@@ -40,19 +39,17 @@ const Form = ({ handleLoginFormSubmit }) => {
         autoComplete="off"
       />
       <button
-        className="form-container__submit"
         onClick={handleLoginClick}
         type="button"
       >
         Login
       </button>
-
     </form>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
 
 Form.propTypes = {
   handleLoginFormSubmit: PropTypes.func.isRequired,
-};
+}
