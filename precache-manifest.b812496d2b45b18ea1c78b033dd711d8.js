a61325ccd47a39a19e6b207d690d355981f47768
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-05-23 18:37:46 +0300

    Updates

---
+++ precache-manifest.b812496d2b45b18ea1c78b033dd711d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6eff15e763fe52880eb8c9458520f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "967976e0e271b0ab37b1",
+    "url": "/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "a3e64389ad48617c6b81",
+    "url": "/static/css/main.fc4137e2.chunk.css"
+  },
+  {
+    "revision": "967976e0e271b0ab37b1",
+    "url": "/static/js/2.786c3e99.chunk.js"
+  },
+  {
+    "revision": "5e41a8f21a28df9b0f9c1bc479bb6432",
+    "url": "/static/js/2.786c3e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e64389ad48617c6b81",
+    "url": "/static/js/main.855f56ec.chunk.js"
+  },
+  {
+    "revision": "d75ef49df9ec69bb6fe9",
+    "url": "/static/js/runtime-main.9d6afc0e.js"
+  },
+  {
+    "revision": "b7dcb8e93e0321db8ba4e9ae6ae87642",
+    "url": "/static/media/logo.b7dcb8e9.svg"
+  }
+]);
