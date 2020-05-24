afbc55fcd4d80de5f338af390fcdb4220acdbabf
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-05-24 23:44:12 +0300

    dep

--- src/components/AddCard/AddCard.js
+++ src/components/AddCard/AddCard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import './addCard.scss'
 
 const AddCard = ({ type, handleClick }) => (
+  // eslint-disable-next-line jsx-a11y/no-static-element-interactions
   <div className="add-card-container" onClick={handleClick}>
     <h2>{`Add a new ${type}`}</h2>
     <i className="fas fa-plus-circle fa-5x" />
