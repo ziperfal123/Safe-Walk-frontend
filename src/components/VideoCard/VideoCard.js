61b0be3cc944e1fc6463236f4b6e542ed8996b09
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-05-27 00:32:53 +0300

    youtube string auto convert added

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {urlToEmbededUrlConvertor} from "utils/youtube";
 import { Card, Popconfirm, message } from 'antd'
 import './videoCard.scss'
 
