97e7c6627625979e3be6564923b39e75771980a8
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-06-06 18:57:17 +0300

    filter added to patients table

--- src/containers/Patients/components/PatientsTable/PatientsTable.js
+++ src/containers/Patients/components/PatientsTable/PatientsTable.js
@@ -1,28 +1,50 @@
-import React from 'react'
-import { Table } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Input, Table } from 'antd'
 import PropTypes from 'prop-types'
+import { cloneDeep } from 'lodash'
 import './patientsTable.scss'
 import columns from './tableColumns'
 
 const PatientsTable = (props) => {
   const { allPatients, handleTableRowClick, loadingAllPatients } = props
+  const [filteredPatients, setFilteredPatients] = useState([])
+
+  useEffect(() => {
+    setFilteredPatients(allPatients)
+  }, [allPatients])
+
   function handleRowClick(patientObj) {
     return {
       onClick: () => handleTableRowClick(patientObj),
     }
   }
 
+  function handleInputChange(e) {
+    let tmpArr = cloneDeep(allPatients)
+    tmpArr = tmpArr.filter((patient) => {
+      const lowerCaseName = patient.name.toLowerCase()
+      return lowerCaseName.includes(e.target.value)
+    })
+    setFilteredPatients(tmpArr)
+  }
+
   return (
-    <div className="patients-table-wrapper">
-      <Table
-        className="table"
-        columns={columns}
-        dataSource={allPatients}
-        pagination={false}
-        onRow={handleRowClick}
-        loading={loadingAllPatients}
-      />
-    </div>
+    <>
+      <div className="search-wrapper">
+        <label>Filter:</label>
+        <Input onChange={handleInputChange} />
+      </div>
+      <div className="patients-table-wrapper">
+        <Table
+          className="table"
+          columns={columns}
+          dataSource={filteredPatients}
+          pagination={false}
+          onRow={handleRowClick}
+          loading={loadingAllPatients}
+        />
+      </div>
+    </>
   )
 }
 
